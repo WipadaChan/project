6e4129747acc4770531aa59dcf77d436b1cb7341
picnicbizz <bizzpicnic@gmail.com>
2014-10-25 17:04:01 +0700

    run_analysisR finish

--- run_analysis.R
+++ run_analysis.R
@@ -7,8 +7,6 @@ trainLabel<-read.table("./dataloc/UCI HAR Dataset/train/Y_train.txt")
 trainSub<-read.table("./dataloc/UCI HAR Dataset/train/subject_train.txt")
 
 
-
-
 #rename column in subject train dataset
 colnames(trainSub)<-"subjecid"
 names(trainSub)
@@ -32,15 +30,18 @@ colnames(testLabel)<-"activityLabel"
 test<-cbind(test,testSub)
 test<-cbind(test,testLabel)
 
-# creat combine dataset from train and test data
+# create a combine dataset from train and test data
 fulldata<-rbind(train,test)
 
+#assign label to columns
 featurs<-read.table("./dataloc/UCI HAR Dataset/features.txt")
-key<-mean()
+# select only mean and standard diviation column
 featurs[grep("mean()|std()", featurs$V2), "sel2"] <- 1
 table(featurs$sel2)
 tem<-ifelse(featurs$sel2==1,featurs$V1,NA)
+# get a vector list of location of which column to be selected
 sel<- tem[!is.na(tem)]
+# generate a vector list of column name
 varnames<-featurs[sel,2]
 
 sel<-c(sel,562, 563)
@@ -53,5 +54,19 @@ colnames(meanStd)[81]<- "activityLabel"
 # add label to activities
 activity<-read.table("./dataloc/UCI HAR Dataset/activity_labels.txt")
 mergelable=merge(meanStd,activity,by.x="activityLabel", by.y="V1",all=FALSE)
-sort(mergelable$activityLabel,mergelable$subjectid)
-tapply(mergelable$activityLabel,mergelable$subjectid, mergelable$tBodyAcc-mean()-X,avg)
+newdata <- mergelable[order(mergelable$subjectid, mergelable$activityLabel),]
+
+
+# the 5th step
+#Generate first aggregate table for first measurment 
+avgData<-aggregate(newdata[,2]~newdata$subjectid+newdata$V2,newdata,mean)
+colnames(avgData)[3]<-as.character(varnames[1])
+
+for (i in 3:80){
+        tem<-aggregate(newdata[,i]~newdata$subjectid+newdata$V2,newdata,mean)
+        colnames(tem)[3]<-as.character(varnames[i-1])
+        avgData=merge(avgData,tem,by =c("newdata$subjectid","newdata$V2"),all=TRUE)
+}
+
+# order a final dataset
+finalDataset <- avgData[order(avgData[,1], avgData[,2]),]
