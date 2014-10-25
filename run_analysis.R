1d4409948f0a073653662a6b3207f05c8a8eb132
picnicbizz <bizzpicnic@gmail.com>
2014-10-25 19:01:35 +0700

    run_analysisR finish

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,7 @@ trainSub<-read.table("./dataloc/UCI HAR Dataset/train/subject_train.txt")
 
 
 #rename column in subject train dataset
-colnames(trainSub)<-"subjecid"
+colnames(trainSub)<-"subjectID"
 names(trainSub)
 # add Subject Identification to the train dataset
 train<-cbind(train,trainSub)
@@ -23,16 +23,18 @@ train<-cbind(train,trainLabel)
 test<-read.table("./dataloc/UCI HAR Dataset/test/X_test.txt")
 testLabel<-read.table("./dataloc/UCI HAR Dataset/test/Y_test.txt")
 testSub<-read.table("./dataloc/UCI HAR Dataset/test/subject_test.txt")
-colnames(testSub)<-"subjecid"
+colnames(testSub)<-"subjectID"
 colnames(testLabel)<-"activityLabel"
 
 # add Subject Identification to the test dataset
 test<-cbind(test,testSub)
 test<-cbind(test,testLabel)
 
+# Step 1:Merges the training and the test sets to create one data set.
 # create a combine dataset from train and test data
 fulldata<-rbind(train,test)
 
+# Step 2: Extracts only the measurements on the mean and standard deviation for each measurement. 
 #assign label to columns
 featurs<-read.table("./dataloc/UCI HAR Dataset/features.txt")
 # select only mean and standard diviation column
@@ -47,29 +49,33 @@ varnames<-featurs[sel,2]
 sel<-c(sel,562, 563)
 # dataset with only mean() and std() of each measurement
 meanStd<-fulldata[,sel]
+
+# Step 3: Appropriately labels the data set with descriptive variable names. 
 colnames(meanStd)<-varnames
-colnames(meanStd)[80]<- "subjectid"
+colnames(meanStd)[80]<- "subjectID"
 colnames(meanStd)[81]<- "activityLabel"
 
+
+#Step 4: Uses descriptive activity names to name the activities in the data set
 # add label to activities
 activity<-read.table("./dataloc/UCI HAR Dataset/activity_labels.txt")
 mergelable=merge(meanStd,activity,by.x="activityLabel", by.y="V1",all=FALSE)
-newdata <- mergelable[order(mergelable$subjectid, mergelable$activityLabel),]
+newdata <- mergelable[order(mergelable$subjectID, mergelable$activityLabel),]
 
 
 # the 5th step
 #Generate first aggregate table for first measurment 
-avgData<-aggregate(newdata[,2]~newdata$subjectid+newdata$V2,newdata,mean)
+avgData<-aggregate(newdata[,2]~newdata$subjectID+newdata$V2,newdata,mean)
 colnames(avgData)[3]<-as.character(varnames[1])
 
 for (i in 3:80){
-        tem<-aggregate(newdata[,i]~newdata$subjectid+newdata$V2,newdata,mean)
+        tem<-aggregate(newdata[,i]~newdata$subjectID+newdata$V2,newdata,mean)
         colnames(tem)[3]<-as.character(varnames[i-1])
-        avgData=merge(avgData,tem,by =c("newdata$subjectid","newdata$V2"),all=TRUE)
+        avgData=merge(avgData,tem,by =c("newdata$subjectID","newdata$V2"),all=TRUE)
 }
 
 # order a final dataset
 finalDataset <- avgData[order(avgData[,1], avgData[,2]),]
-colnames(finalDataset)[1]<-"Subjectid"
+colnames(finalDataset)[1]<-"subjectID"
 colnames(finalDataset)[2]<-"Activity"
 write.table(finalDataset,file="./dataloc/UCI HAR Dataset/sensor_avg_by_act_sub.txt",row.name=FALSE)
